0ba4d513424da5f0af67ea6450146e2e6d58bc79
Ayodeji Abolaji <abolajiayodeji93@gmail.com>
2023-05-12 04:01:58 +0100

    I adjusted my javascript code

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-document.getElementById("program").innerHTML = "I am currently learning JavaScript";
+const btn = document.querySelector(".btn");
+const text = document.getElementById("program");
+
+// JavaScriptDOM element
+btn.addEventListener("click", function () {
+  text.innerHTML = "I am currently learning JavaScript";
+});
